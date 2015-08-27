7bb92955888edfcf9a8aa6fc2d9d21893a8381aa
Alex <lyosha85dev@gmail.com>
2015-08-27 13:41:36 +0900

    passing params with sinatra

--- sinatra/jumpstart/01_01_hello.rb
+++ sinatra/jumpstart/01_01_hello.rb
@@ -17,3 +17,17 @@ end
 get '/:name' do
   "Hai there,.. #{params[:name]}" # access params hash like in rails
 end
+
+# params passing
+# /frank/sinatra/here takes precedence over /frank
+get "/:one/:two/:three" do
+  "first: #{params[:one]} <br/>" +
+  "second: #{params[:two]} <br/>" +
+  "third: #{params[:three]} <br/>"
+end
+
+get "/what/time/is/it/in/:number/hours" do
+  number = params[:number].to_i
+  time = Time.now + number * 3600
+  "The time in #{number} hours will be #{time.strftime('%I:%M %p')}"
+end
