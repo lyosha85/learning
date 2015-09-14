fdf519c07de11981c1652b4e8108fee36f810db4
Alex <lyosha85dev@gmail.com>
2015-09-14 12:21:57 +0900

    public variables - that can be overwritten

--- js/js-oop/2-modules/module_hyp.js
+++ js/js-oop/2-modules/module_hyp.js
@@ -2,7 +2,22 @@
 // Hypothetical Example
 
 var jill = (function(){
-  return {
 
+  // private variables that cant be accessed outside this function
+  var first_name = "jill";
+  var last_name = "tylor";
+  age = 28;
+  interests = [];
+
+  function get_public_age() {
+    return age - 5;
+  }
+
+  // only to be accessed after the function has been returned
+  return {
+    full_name: function(){
+      return first_name + ' ' + last_name
+    },
+    age: get_public_age
   };
 });
