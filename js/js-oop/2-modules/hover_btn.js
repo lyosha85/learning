cb38f496502cb0d3337818fb5ea1ebe7d9cf3d2d
Alex <lyosha85dev@gmail.com>
2015-09-14 15:19:57 +0900

    complete module pattern

---
+++ js/js-oop/2-modules/hover_btn.js
@@ -0,0 +1,35 @@
+var hover_btn = (function(){
+  var btn = jQuery("#button"),
+      panel = jQuery("#panel");
+
+  function fade_in() {
+    panel.fadeIn();
+  }
+
+  function fade_out() {
+    panel.fadeOut();
+  }
+
+  return {
+    bind_events : function() {
+      btn.bind("mouseover", fade_in)
+         .bind("mouseout", fade_out);
+      return this;
+    },
+    unbind_events : function() {
+      btn.unbind("mouseover", fade_in)
+         .unbind("mouseout", fade_out);
+      return this;
+    },
+    show : function(){
+      fade_in();
+      return this;
+    },
+    hide : function(){
+      fade_out();
+      return this;
+    }
+  }
+}());
+
+hover_btn.bind_events();
