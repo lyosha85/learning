e9969270d825a5ee848f3f2426afe013f83e01dc
Alex <lyosha85dev@gmail.com>
2015-09-14 12:42:04 +0900

    show difference between module public and private methods / vars

--- js/js-oop/2-modules/module_hyp.js
+++ js/js-oop/2-modules/module_hyp.js
@@ -4,10 +4,10 @@
 var jill = (function(){
 
   // private variables that cant be accessed outside this function
-  var first_name = "jill";
-  var last_name = "tylor";
-  age = 28;
-  interests = [];
+  var first_name = "jill",
+      last_name = "tylor",
+      age = 28,
+      interests = [];
 
   function get_public_age() {
     return age - 5;
@@ -18,6 +18,23 @@ var jill = (function(){
     full_name: function(){
       return first_name + ' ' + last_name
     },
-    age: get_public_age
+    age: get_public_age,
+    greet: function() {
+      return "hello, my name is " + this.full_name() + "I am " + get_public_age()
+    },
+    interests: function() {
+      return interests;
+    },
+    add_interests: function() {
+      for(var i = 0; i < arguments.length; i++){
+        interests.push(arguments[i]);
+      }
+    }
   };
-});
+}());
+
+console.log(jill.full_name());
+console.log(jill.age());
+console.log(jill.interests());
+jill.add_interests("web design", "skating");
+console.log(jill.interests());
