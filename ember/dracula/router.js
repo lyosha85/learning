20f39d613b7224e41f3a5043d953e538f1e4cf41
Alex <lyosha85@gmail.com>
2015-09-16 19:28:06 +0900

    add 2 nested routes for contact

--- ember/dracula/router.js
+++ ember/dracula/router.js
@@ -1,5 +1,8 @@
 Blogger.Router.map( function() {
   this.resource('posts', {path: '/'});
   this.resource('about', {path: 'about-us'});
-  this.resource('contact', {path: 'contact'});
+  this.resource('contact', {path: 'contact'}, function(){
+  	this.resource('phone');
+  	this.resource('email');
+  });
 });
