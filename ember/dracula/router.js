4b65beaebe0893f7db6892b886dfc458e580ed21
Alex <lyosha85@gmail.com>
2015-09-16 19:11:37 +0900

    add custom paths for other pages too

--- ember/dracula/router.js
+++ ember/dracula/router.js
@@ -1,5 +1,5 @@
 Blogger.Router.map( function() {
   this.resource('posts', {path: '/'});
-  this.resource('about');
-  this.resource('contact');
+  this.resource('about', {path: 'about-us'});
+  this.resource('contact', {path: 'contact'});
 });
