2141c4fd5ae3050d28b689d32b1eee1383d32876
Alex <lyosha85@gmail.com>
2015-09-18 01:04:10 +0900

    format the data to be easier to parse with ember

--- ember/dracula/store.js
+++ ember/dracula/store.js
@@ -19,22 +19,22 @@ var posts = [
 var comments = [
 	{
 		id: '1',
-		post_id: '1',
-		comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod"
+		postId: '1',
+		text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod"
 	},
 	{
 		id: '2',
-		post_id: '1',
-		comment: "proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+		postId: '1',
+		text: "proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 	},
 	{
 		id: '3',
-		post_id: '2',
-		comment: "cillum dolore eu  pariatur. Excepteur sint occaecat cupidatat no"
+		postId: '2',
+		text: "cillum dolore eu  pariatur. Excepteur sint occaecat cupidatat no"
 	},
 	{
 		id: '4',
-		post_id: '3',
-		comment: "Duis aute irure dolor in reprehenderit in voluptate velit esse"
+		postId: '3',
+		text: "Duis aute irure dolor in reprehenderit in voluptate velit esse"
 	}
 ]
