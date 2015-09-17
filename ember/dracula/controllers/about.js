546ee559687a25aaf42b4c2cc3a14b336206c3d9
Alex <lyosha85@gmail.com>
2015-09-17 23:13:50 +0900

    create show and hide picture, to set a new property, you have to use this.set(property, value)

--- ember/dracula/controllers/about.js
+++ ember/dracula/controllers/about.js
@@ -1,7 +1,12 @@
 Blogger.AboutController = Ember.Controller.extend({
+	isPictureShowing: false,
 	actions: {
 		showRealName: function(){
 			alert('Wallachian');
+		},
+		showPicture: function(){
+			this.set('isPictureShowing', true);
 		}
+		
 	}
 });
