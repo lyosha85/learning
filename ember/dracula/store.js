a57c68ddd8d740cb8149b1516c06f5ddd0786b39
Alex <lyosha85@gmail.com>
2015-09-18 00:02:26 +0900

    add some sample data to mess around with models

---
+++ ember/dracula/store.js
@@ -0,0 +1,40 @@
+var posts = [
+	{
+		id: '1',
+		title: 'I love blood !',
+		body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus aspernatur tempora odit? Officiis perferendis consequatur molestias recusandae tempore quod alias quam delectus harum, error dolorem atque, deleniti hic repellendus iste.'
+	},
+	{
+		id: '2',
+		title: 'The count',
+		body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus aspernatur tempora odit? Officiis perferendis consequatur molestias recusandae tempore quod alias quam delectus harum, error dolorem atque, deleniti hic repellendus iste.'
+	},
+	{
+		id: '3',
+		title: 'Labas Vakaras',
+		body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus aspernatur tempora odit? Officiis perferendis consequatur molestias recusandae tempore quod alias quam delectus harum, error dolorem atque, deleniti hic repellendus iste.'
+	},
+]
+
+var comments = [
+	{
+		id: '1',
+		post_id: '1',
+		comment: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod"
+	},
+	{
+		id: '2',
+		post_id: '1',
+		comment: "proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+	},
+	{
+		id: '3',
+		post_id: '2',
+		comment: "cillum dolore eu  pariatur. Excepteur sint occaecat cupidatat no"
+	},
+	{
+		id: '4',
+		post_id: '3',
+		comment: "Duis aute irure dolor in reprehenderit in voluptate velit esse"
+	}
+]
