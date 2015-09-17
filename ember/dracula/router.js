daa740e4863a3662ee862dcea7caa0cd63912190
Alex <lyosha85@gmail.com>
2015-09-18 01:31:18 +0900

    add links to individual posts :D

--- ember/dracula/router.js
+++ ember/dracula/router.js
@@ -1,5 +1,6 @@
 Blogger.Router.map( function() {
   this.resource('posts', {path: '/'});
+  this.resource('post', {path: 'post/:post_id'});
   this.resource('about', {path: 'about-us'});
   this.resource('contact', {path: 'contact'}, function(){
   	this.resource('phone');
