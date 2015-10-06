56144de63e65d9f969c20d112ccf3ac8e7b6b26f
Alex <lyosha85@gmail.com>
2015-10-06 17:43:29 +0900

    prep ember for testing (http://instructure.github.io/blog/2014/01/24/ember-run-loop-and-tdd/)

--- ember/address-book/test/javascripts/test_helper.js
+++ ember/address-book/test/javascripts/test_helper.js
@@ -28,3 +28,12 @@
 //
 // For more information: http://github.com/modeset/teaspoon
 
+
+//= require_self
+
+var d = document;
+d.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
+
+AddressBook.rootElement = "#ember-testing";
+AddressBook.setupForTesting();
+AddressBook.injectTestHelpers();
