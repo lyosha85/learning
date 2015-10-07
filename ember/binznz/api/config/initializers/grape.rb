ae6a0b16e84d2887e7078f1ad9af229ed8dedff3
Alex <lyosha85@gmail.com>
2015-10-07 16:58:45 +0900

    oopse

--- ember/binznz/api/config/initializers/grape.rb
+++ ember/binznz/api/config/initializers/grape.rb
@@ -1,3 +1,3 @@
 GrapeSwaggerRails.options.url = "api/v1/swagger_doc"
-GrapeSwaggerRails.options.url = "Binznz"
+GrapeSwaggerRails.options.app_name = "Binznz"
 GrapeSwaggerRails.options.app_url = "http://localhost:3000"
