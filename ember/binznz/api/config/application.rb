1903c638118a0e02c7609122f587846ff249b90f
Alex <lyosha85@gmail.com>
2015-10-07 16:22:06 +0900

    cors!

--- ember/binznz/api/config/application.rb
+++ ember/binznz/api/config/application.rb
@@ -31,5 +31,15 @@ module Api
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # allow cors
+    # http://www.adobe.com/devnet/archive/html5/articles/understanding-cross-origin-resource-sharing-cors.html
+    
+    config.middleware.use Rack::Cors do
+      allow do
+        origins "*"
+        resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options]
+      end
+    end
   end
 end
