76a2ab70d348212333374e8e9033e6cb40352cee
Alex <lyosha85@gmail.com>
2015-12-11 21:40:29 +0900

    rental stating point

--- ruby/refactoring-ruby/ch1_movie_rental.rb
+++ ruby/refactoring-ruby/ch1_movie_rental.rb
@@ -10,3 +10,11 @@ class Movie
 		@title, @price_code = title, price_code
 	end
 end
+
+class Rental
+	attr_reader :movie, :days_rented
+
+	def initialize(movie, days_rented)
+		@movie, @days_rented = movie, days_rented
+	end
+end
