59261e6cb17173fd23d888faefc1d25ce4fda0d7
Alex <lyosha85@gmail.com>
2015-12-18 00:43:59 +0900

    quick tests for refactoring

--- ruby/refactoring-ruby/ch1-first-example/spec/ch1_movie_rental_spec.rb
+++ ruby/refactoring-ruby/ch1-first-example/spec/ch1_movie_rental_spec.rb
@@ -1,1 +1,31 @@
 require 'ch1_movie_rental'
+
+# RSpec.describe Customer do
+# 	describe '#add_rental' do
+# 		let(:movie) { Movie.new("Fight Club", 100 ) }
+# 		let(:rental)
+# 		it 'should add a movie' do
+# 			before 
+# 		end
+# 	end
+# end
+
+RSpec.describe Movie do
+	describe "#new" do
+		let(:movie) { Movie.new("Fight Club", 100) }
+		it { movie.title == "Fight Club"}
+		it { movie.price_code == 100 }
+	end
+end	
+
+RSpec.describe Rental do
+	describe "#new" do
+		let(:rental) { Rental.new( Movie.new("Starwars", 102), 6 ) }
+		it {rental.movie.should be_a Movie}
+		it {rental.days_rented == 6}
+	end
+end
+
+RSpec.describe Customer do
+
+end
