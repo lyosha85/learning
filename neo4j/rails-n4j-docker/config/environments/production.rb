84b622770f4e9ea678bad8decfc49a66c92e689d
Alex <lyosha85@gmail.com>
2016-01-01 00:33:57 +0900

    add neo4j production path and require neo4j

--- neo4j/rails-n4j-docker/config/environments/production.rb
+++ neo4j/rails-n4j-docker/config/environments/production.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.neo4j.session_path = ENV['NEO4J_URL']
 end
