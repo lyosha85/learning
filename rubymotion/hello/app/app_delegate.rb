33d9093b0a328ee462ece669fa22854b62028e26
Alex <lyosha85@gmail.com>
2016-01-29 12:05:44 +0900

    display alert in ios

--- rubymotion/hello/app/app_delegate.rb
+++ rubymotion/hello/app/app_delegate.rb
@@ -1,15 +1,12 @@
+# This file implements the AppDelegate class, which is responsible for controlling your application.
+
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    rootViewController = UIViewController.alloc.init
-    rootViewController.title = 'hello'
-    rootViewController.view.backgroundColor = UIColor.whiteColor
+    alert = UIAlertView.new
+    alert.message = "Hello World!"
+    alert.show
 
-    navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
-
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    @window.rootViewController = navigationController
-    @window.makeKeyAndVisible
-
-    true
+    # true
+    false
   end
 end
