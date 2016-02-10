471158659494b861d69c39e209a90efaa619bb0f
Alex <lyosha85@gmail.com>
2016-02-10 17:53:16 +0900

    minor

--- activescaffold/blog/config/routes.rb
+++ activescaffold/blog/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users do as_routes end
   resources :posts
-  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
