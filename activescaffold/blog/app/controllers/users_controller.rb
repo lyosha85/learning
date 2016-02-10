9b9ae7563e2da40f87488aec43c71eada19740fc
Alex <lyosha85@gmail.com>
2016-02-10 17:51:39 +0900

    add a filter by column value

--- activescaffold/blog/app/controllers/users_controller.rb
+++ activescaffold/blog/app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
   active_scaffold :"user" do |conf|
+
+  end
+  active_scaffold do |config|
+    config.action_links.add :index, :label => 'Show active', :parameters => {:active => true}, :position => false
   end
 end
