7c80c10733be819b8f0f0db6cb47bc7bca2599cc
Alex <lyosha85@gmail.com>
2016-02-10 17:53:00 +0900

    filter by possible scopes

--- activescaffold/blog/app/controllers/users_controller.rb
+++ activescaffold/blog/app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
     config.action_links.add :index, :label => 'Show active', :parameters => {:scope => :a}, :position => false
   end
   def beginning_of_chain
-    super.send(params[:scope]||:all)
+    if params[:scope].present? && User::POSSIBLE_SCOPES.include?(params[:scope])
+      super.send(params[:scope] )
+    end
   end
 end
